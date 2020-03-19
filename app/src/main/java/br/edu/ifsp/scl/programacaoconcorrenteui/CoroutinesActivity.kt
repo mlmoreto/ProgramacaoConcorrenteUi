56b35586bfbebd71174d97bd42820f959f59ad7a
mlmoreto <marialauramoreto@hotmail.com>
2020-03-19 12:17:47 -0300

    Implementacao da corrotina do Kotlin

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -2,11 +2,26 @@ package br.edu.ifsp.scl.programacaoconcorrenteui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
-class CoroutinesActivity : AppCompatActivity() {
+class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
+
+    val supervisorJob = SupervisorJob();
+    val coroutineScope = CoroutineScope(Dispatchers.Default + supervisorJob)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutines)
+    }
+
+    override fun run() {
+        coroutineScope.launch {
+            altaIv.setImageResource(R.drawable.android_preto)
+            baixaIv.setImageResource(R.drawable.android_verde)
+        }
     }
 }
